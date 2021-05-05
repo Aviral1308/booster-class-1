41ee3727c4e44cf805adef760340b5656feb2993
Aviral1308 <74609945+Aviral1308@users.noreply.github.com>
2021-05-05 17:58:14 +0530

    Add files via upload

---
+++ Js/Form.js
@@ -0,0 +1,43 @@
+class Form{
+    constructor(){
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+    this.title = createElement('h2');
+ }
+    hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+    }
+
+    display(){
+     
+        this.title.html("Maze Game");
+        this.title.position(displayWidth/2 - 50, 0);
+    
+        this.input.position(displayWidth/2 - 100 , displayHeight/2 - 140);
+        this.button.position(displayWidth/2 + 80, displayHeight/2 - 140);
+        //this.reset.position(displayWidth-100,20);
+    
+        this.button.mousePressed(()=>{
+          this.input.hide();
+          this.button.hide();
+          this.title.hide();
+          player.name = this.input.value();
+          playerCount+=1;
+          player.index = playerCount;
+          player.update();
+          player.updateCount(playerCount);
+          this.greeting.html("Hello " + player.name)
+          this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+     });
+    
+        //this.reset.mousePressed(()=>{
+        //  player.updateCount(0);
+        //  game.update(0);
+         // Player.getCarsAtEnd(0);
+      //  });
+      }
+}
